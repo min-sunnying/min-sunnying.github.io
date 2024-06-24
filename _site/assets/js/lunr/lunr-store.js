4d39062a4f3286052baa59d8aa4244459b894c4b
min-sunnying <9909cindy@kaist.ac.kr>
2024-06-24 13:57:24 +0900

    news

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,12 +1,18 @@
 var store = [{
         "title": "RECIPE: How to Integrate ChatGPT into EFL Writing Education",
-        "excerpt":"Authors: Jieun Han, Haneul Yoo, Yoonsu Kim, Junho Myung, Minsun Kim, Hyunseung Lim, Juho Kim, Tak Yeon Lee, Hwajung Hong, So-Yeon Ahn, Alice Oh Journal: arXiv preprint Abstract: The integration of generative AI in the field of education is actively being explored. In particular, ChatGPT has garnered significant interest, offering...","categories": ["Publication"],
+        "excerpt":"Authors: Jieun Han, Haneul Yoo, Yoonsu Kim, Junho Myung, Minsun Kim, Hyunseung Lim, Juho Kim, Tak Yeon Lee, Hwajung Hong, So-Yeon Ahn, Alice Oh Journal: Proceedings of the Tenth ACM Conference on Learning@ Scale Abstract: The integration of generative AI in the field of education is actively being explored. In...","categories": ["Publication"],
         "tags": [],
         "url": "/publication/RECIPE/",
         "teaser": null
       },{
-        "title": "Designing Prompt Analytics Dashboards to Analyze Student-ChatGPT Interactions in EFL Writing",
-        "excerpt":"Authors: Minsun Kim, SeonGyeom Kim, Suyoun Lee, Yoosang Yoon, Junho Myung, Haneul Yoo, Hyungseung Lim, Jieun Han, Yoonsu Kim, So-Yeon Ahn, Juho Kim, Alice Oh, Hwajung Hong, Tak Yeon Lee Journal: arXiv preprint Abstract: While ChatGPT has significantly impacted education by offering personalized resources for students, its integration into educational...","categories": ["Publication"],
+        "title": "Master Thesis Proposal!",
+        "excerpt":"  ","categories": ["Publication","News"],
+        "tags": [],
+        "url": "/publication/news/masterproposal/",
+        "teaser": null
+      },{
+        "title": "Exploring Student-ChatGPT Dialogue in EFL Writing Education",
+        "excerpt":"Authors: Jieun Han, Haneul Yoo, Junho Myung, Minsun Kim, Tak Yeon Lee, So-Yeon Ahn, Alice Oh Journal: NeurIPS’23 Workshop on Generative AI for Education (GAIED) Abstract: The integration of generative AI in education is expanding, yet empirical analyses of large-scale and real-world interactions between students and AI systems still remain...","categories": ["Publication"],
         "tags": [],
         "url": "/publication/RECIPE4U/",
         "teaser": null
@@ -15,5 +21,11 @@ var store = [{
         "excerpt":"Authors: Minsun Kim, SeonGyeom Kim, Suyoun Lee, Yoosang Yoon, Junho Myung, Haneul Yoo, Hyungseung Lim, Jieun Han, Yoonsu Kim, So-Yeon Ahn, Juho Kim, Alice Oh, Hwajung Hong, Tak Yeon Lee Journal: arXiv preprint Abstract: While ChatGPT has significantly impacted education by offering personalized resources for students, its integration into educational...","categories": ["Publication"],
         "tags": [],
         "url": "/publication/PAD/",
+        "teaser": null
+      },{
+        "title": "Defense",
+        "excerpt":" ","categories": ["Publication"],
+        "tags": [],
+        "url": "/publication/defense/",
         "teaser": null
       },]
