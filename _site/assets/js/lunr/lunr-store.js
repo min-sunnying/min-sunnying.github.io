4e9ad665b88579e03deaa5ad2771a05e883cf513
min-sunnying <9909cindy@kaist.ac.kr>
2024-06-24 13:36:43 +0900

    post-1

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,14 +1,14 @@
 var store = [{
-        "title": "Designing Prompt Analytics Dashboards to Analyze Student-ChatGPT Interactions in EFL Writing",
-        "excerpt":"Authors: Minsun Kim, SeonGyeom Kim, Suyoun Lee, Yoosang Yoon, Junho Myung, Haneul Yoo, Hyungseung Lim, Jieun Han, Yoonsu Kim, So-Yeon Ahn, Juho Kim, Alice Oh, Hwajung Hong, Tak Yeon Lee Journal: arXiv preprint Abstract: While ChatGPT has significantly impacted education by offering personalized resources for students, its integration into educational...","categories": ["Publication"],
+        "title": "RECIPE: How to Integrate ChatGPT into EFL Writing Education",
+        "excerpt":"Authors: Jieun Han, Haneul Yoo, Yoonsu Kim, Junho Myung, Minsun Kim, Hyunseung Lim, Juho Kim, Tak Yeon Lee, Hwajung Hong, So-Yeon Ahn, Alice Oh Journal: arXiv preprint Abstract: The integration of generative AI in the field of education is actively being explored. In particular, ChatGPT has garnered significant interest, offering...","categories": ["Publication"],
         "tags": [],
-        "url": "/publication/PAD-copy-2/",
+        "url": "/publication/RECIPE/",
         "teaser": null
       },{
         "title": "Designing Prompt Analytics Dashboards to Analyze Student-ChatGPT Interactions in EFL Writing",
         "excerpt":"Authors: Minsun Kim, SeonGyeom Kim, Suyoun Lee, Yoosang Yoon, Junho Myung, Haneul Yoo, Hyungseung Lim, Jieun Han, Yoonsu Kim, So-Yeon Ahn, Juho Kim, Alice Oh, Hwajung Hong, Tak Yeon Lee Journal: arXiv preprint Abstract: While ChatGPT has significantly impacted education by offering personalized resources for students, its integration into educational...","categories": ["Publication"],
         "tags": [],
-        "url": "/publication/PAD-copy/",
+        "url": "/publication/RECIPE4U/",
         "teaser": null
       },{
         "title": "Designing Prompt Analytics Dashboards to Analyze Student-ChatGPT Interactions in EFL Writing",
